8e5beb8bf94e0efd901345b0c3340a8c0e33e764
kienhoang3397 <125807253+kienhoang3397@users.noreply.github.com>
2025-05-08 22:14:15 +0700

    feat: add DefaultLayout and HomeLayout components for improved page structure and layout management

--- app/page.tsx
+++ app/page.tsx
@@ -13,6 +13,7 @@ import Newsletter from "@/components/sections/newsletter-section";
 import { TestimonialsSection } from "@/components/sections/testimonials-section";
 import TextMarqueeSection from "@/components/sections/text-marquee-section";
 import { DemoUISection } from "@/components/sections/demo-ui-section";
+import HomeLayout from './home-layout';
 
 export default function HomePage() {
   useEffect(() => {
@@ -30,47 +31,49 @@ export default function HomePage() {
   }, []);
 
   return (
-    <main className="overflow-hidden w-full">
-      {/* to hon */}
-      {/* <SplashCursor
-        SIM_RESOLUTION={128}
-        DYE_RESOLUTION={768}
-        DENSITY_DISSIPATION={6}
-        VELOCITY_DISSIPATION={3.5}
-        PRESSURE={0.4}
-        PRESSURE_ITERATIONS={20}
-        CURL={15}
-        SPLAT_RADIUS={0.15}
-        SPLAT_FORCE={4000}
-        SHADING={true}
-        COLOR_UPDATE_SPEED={8}
-        BACK_COLOR={{ r: 0, g: 0, b: 0 }}
-        TRANSPARENT={true}
-      /> */}
-      <SplashCursor
-        SIM_RESOLUTION={128}
-        DYE_RESOLUTION={512}
-        DENSITY_DISSIPATION={9}
-        VELOCITY_DISSIPATION={5}
-        PRESSURE={0.25}
-        PRESSURE_ITERATIONS={15}
-        CURL={8}
-        SPLAT_RADIUS={0.1}
-        SPLAT_FORCE={2500}
-        SHADING={true}
-        COLOR_UPDATE_SPEED={6}
-        BACK_COLOR={{ r: 0, g: 0, b: 0 }}
-        TRANSPARENT={true}
-      />
-      <HeroSection />
-      <WobbleCardSection />
-      {/* <BentoGrid /> */}
-      <TestimonialsSection />
-      <TextMarqueeSection />
-      <Newsletter />
-      <DemoUISection />
-      <Faq02 />
-      <Footer />
-    </main>
+    <HomeLayout>
+      <main className="min-h-screen">
+        {/* to hon */}
+        {/* <SplashCursor
+          SIM_RESOLUTION={128}
+          DYE_RESOLUTION={768}
+          DENSITY_DISSIPATION={6}
+          VELOCITY_DISSIPATION={3.5}
+          PRESSURE={0.4}
+          PRESSURE_ITERATIONS={20}
+          CURL={15}
+          SPLAT_RADIUS={0.15}
+          SPLAT_FORCE={4000}
+          SHADING={true}
+          COLOR_UPDATE_SPEED={8}
+          BACK_COLOR={{ r: 0, g: 0, b: 0 }}
+          TRANSPARENT={true}
+        /> */}
+        <SplashCursor
+          SIM_RESOLUTION={128}
+          DYE_RESOLUTION={512}
+          DENSITY_DISSIPATION={9}
+          VELOCITY_DISSIPATION={5}
+          PRESSURE={0.25}
+          PRESSURE_ITERATIONS={15}
+          CURL={8}
+          SPLAT_RADIUS={0.1}
+          SPLAT_FORCE={2500}
+          SHADING={true}
+          COLOR_UPDATE_SPEED={6}
+          BACK_COLOR={{ r: 0, g: 0, b: 0 }}
+          TRANSPARENT={true}
+        />
+        <HeroSection />
+        <WobbleCardSection />
+        {/* <BentoGrid /> */}
+        <TestimonialsSection />
+        <TextMarqueeSection />
+        <Newsletter />
+        <DemoUISection />
+        <Faq02 />
+        <Footer />
+      </main>
+    </HomeLayout>
   );
 }
