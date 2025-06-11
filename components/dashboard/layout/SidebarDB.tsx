d0f2b5f29de05510fb0e1172b358833f39eefca2
Quankzz <165643643+Quankzz@users.noreply.github.com>
2025-06-11 15:53:13 +0700

    chore: sidebarDB

--- components/dashboard/layout/SidebarDB.tsx
+++ components/dashboard/layout/SidebarDB.tsx
@@ -11,14 +11,13 @@ import { NavGroup } from '@/components/dashboard/layout/NavGroup';
 import { NavUser } from '@/components/dashboard/layout/NavUser';
 import Logo from '@/components/common/logo';
 import { useAuth } from '@/contexts/auth-context';
-import { sidebarData } from '@/components/dashboard/data/sidebarData';
-
+import { useGenerateSidebarData } from '../data/sidebarData';
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
   const { user } = useAuth();
 
   // Generate sidebar data based on current user using hook with language context
-
+  const sidebarData = useGenerateSidebarData(user);
 
   return (
     <Sidebar collapsible='icon' variant='floating' {...props}>
